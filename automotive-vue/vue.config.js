52f84ae775bdefe2f4a2fb9af35425ac463a5764
Kamil Balcerzak <kbalcerek@o2.pl>
2020-03-23 17:46:06 +0100

    FIBO-110 Create automotive-vue - chanded default url path to start from /auto
    Signed-off-by: Kamil Balcerzak <kbalcerek@o2.pl>

--- automotive-vue/vue.config.js
+++ automotive-vue/vue.config.js
@@ -3,7 +3,7 @@ process.env.VUE_APP_BRANCH    = process.env.product_branch_tag && process.env.pr
 process.env.VUE_APP_TAG       = process.env.product_branch_tag && process.env.product_branch_tag.split('/',3)[2] || 'latest'
 
 module.exports = {
-  publicPath: `/${process.env.ONTPUB_FAMILY || 'fibo'}/`,
+  publicPath: `/${process.env.ONTPUB_FAMILY || 'auto'}/`,
   assetsDir: process.env.product_branch_tag || 'htmlpages/master/latest',
   indexPath: `${process.env.product_branch_tag || 'htmlpages/master/latest'}/index.html`,
   css: {
